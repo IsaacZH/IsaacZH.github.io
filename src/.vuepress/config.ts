f7d152ec6f25639b36d794f20479227fa452e16e
Isaac <1812924685@qq.com>
2024-09-03 21:42:36 +0800

    change base again

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/IsaacZH.github.io/",
+  base: "/",
 
   lang: "en-US",
   title: "Isaac",
